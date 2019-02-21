0854960af34d947b70b642c3eda39070315a56c3
Vu Hai Lam <lamvh2812@gmail.com>
2019-02-21 21:18:41 +0700

    add build Cmake folder

---
+++ build/CMakeFiles/pro_and_con.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/ADMIN/Desktop/asyn_1_thread/Consumer.cpp" "C:/Users/ADMIN/Desktop/asyn_1_thread/build/CMakeFiles/pro_and_con.dir/Consumer.cpp.obj"
+  "C:/Users/ADMIN/Desktop/asyn_1_thread/Producer.cpp" "C:/Users/ADMIN/Desktop/asyn_1_thread/build/CMakeFiles/pro_and_con.dir/Producer.cpp.obj"
+  "C:/Users/ADMIN/Desktop/asyn_1_thread/main.cpp" "C:/Users/ADMIN/Desktop/asyn_1_thread/build/CMakeFiles/pro_and_con.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pro_and_con.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pro_and_con.dir/Producer.cpp.obj"
+  "CMakeFiles/pro_and_con.dir/Consumer.cpp.obj"
+  "CMakeFiles/pro_and_con.dir/main.cpp.obj"
+  "pro_and_con.pdb"
+  "pro_and_con.exe"
+  "pro_and_con.exe.manifest"
+  "libpro_and_con.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pro_and_con.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
